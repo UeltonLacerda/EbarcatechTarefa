c0570726cec0cd30f0133c8b8f416700b7e9fd06
UeltonLacerda <lacerda.uelton@gmail.com>
2024-12-22 18:16:38 -0300

    Segunda alteração fieita para envio

--- massa.c
+++ massa.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 int main() 
 
 {
-  char operacao;
+  char operacao;                 //Modificado 
   
   float numero;
   
-  	printf("Escolha a opcao: \n");//Modifiquei para teste
+  	printf("Escolha uma opcao para conversao: \n\n");
 	printf("Digite [1] para conversao de tonelada para quilo: \n");
 	printf("Digite [2] para conversao de tonelada para grama: \n");
 	printf("Digite [3] para conversao de quilo para tonelada: \n");
@@ -15,14 +16,14 @@ int main()
 	printf("Digite [6] para conversao de grama para tonelada: \n");
     scanf("%c",&operacao);
   
-  printf("Escolha uma opcao para a conversao: ");
+  printf("Digite um valor para a conversao: ");
   scanf("%f",&numero);
   
   
   switch( operacao ) {
   	
 	case '1': printf("%.2f * 1000 = %.2f quilos", numero, numero * 1000); break;
-	case '2': printf("%.2f * 1000000 = %.4f toneladas", numero, numero * 1000000); break;
+	case '2': printf("%.2f * 1000000 = %.4f gramas", numero, numero * 1000000); break;
 	case '3': printf("%.2f / 1000 = %.2f toneladas", numero, numero / 1000); break;
     case '4': printf("%.2f * 1000 = %.2f gramas", numero, numero * 1000); break;
   	case '5': printf("%.2f / 1000 = %.2f quilos", numero, numero / 1000); break;
--- output/UniMassa.c
+++ output/UniMassa.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 int main() 
 
 {
-  char operacao;
+  char operacao;                 //Modificado 
   
   float numero;
   
