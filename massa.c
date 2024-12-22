6010f68c3f31c83cb9a9b36e10be05d670cff18e
UeltonLacerda <lacerda.uelton@gmail.com>
2024-12-22 16:43:28 -0300

    Modifiquei o arquivo só com comentario

--- massa.c
+++ massa.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
+int main() 
 
+{
+  char operacao;
+  
+  float numero;
+  
+  	printf("Escolha a opcao: \n");//Modifiquei para teste
+	printf("Digite [1] para conversao de tonelada para quilo: \n");
+	printf("Digite [2] para conversao de tonelada para grama: \n");
+	printf("Digite [3] para conversao de quilo para tonelada: \n");
+	printf("Digite [4] para conversao de quilo para grama: \n");
+	printf("Digite [5] para conversao de grama para quilo: \n");
+	printf("Digite [6] para conversao de grama para tonelada: \n");
+    scanf("%c",&operacao);
+  
+  printf("Escolha uma opcao para a conversao: ");
+  scanf("%f",&numero);
+  
+  
+  switch( operacao ) {
+  	
+	case '1': printf("%.2f * 1000 = %.2f quilos", numero, numero * 1000); break;
+	case '2': printf("%.2f * 1000000 = %.4f toneladas", numero, numero * 1000000); break;
+	case '3': printf("%.2f / 1000 = %.2f toneladas", numero, numero / 1000); break;
+    case '4': printf("%.2f * 1000 = %.2f gramas", numero, numero * 1000); break;
+  	case '5': printf("%.2f / 1000 = %.2f quilos", numero, numero / 1000); break;
+  	case '6': printf("%.2f / 1000000 = %.4f toneladas", numero, numero / 1000000); break;
+  	default: printf("Você digitou uma operacao invalida.");
+}
 
-int main(){
-	
-	double valor, grama, tonelada;
-	
-	printf("Digite um valor em quilograma para conversao: ");
-	scanf("%lf", &valor);
-	
-	tonelada = valor / 1000;
-    grama = valor * 1000;
-
-
-	
-	printf(" O valor em toneladas eh: %lf\n", tonelada);
-    printf(" O valor em gramas eh: %lf\n", grama);
-	
 }
